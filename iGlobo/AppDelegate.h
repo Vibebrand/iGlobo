83f0e2a9b0f35737fb641fd53fd06a12bf6e3e7f
Jesus Cagide Alvarado <set311@gmail.com>
2011-11-16 09:50:59 -0600

    Initial Commit

---
+++ iGlobo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iGlobo
+//
+//  Created by Jesus Cagide on 16/11/11.
+//  Copyright (c) 2011 INEGI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iGlobo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iGlobo
+//
+//  Created by Jesus Cagide on 16/11/11.
+//  Copyright (c) 2011 INEGI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
