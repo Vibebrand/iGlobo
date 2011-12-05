c1560a3ab5d83e1c0f59298d8afa484cd67041b8
Jesus Cagide Alvarado <set311@gmail.com>
2011-12-05 12:33:28 -0600

    agregando motor a la parte visual

---
+++ iGlobo/ControladorMotorIMapa.h
@@ -0,0 +1,13 @@
+//
+//  ControladorMotorIMapa.h
+//  iGlobo
+//
+//  Created by Jesus Cagide on 05/12/11.
+//  Copyright (c) 2011 INEGI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ControladorMotorIMapa : NSObject
+
+@end
---
+++ iGlobo/ControladorMotorIMapa.mm
@@ -0,0 +1,13 @@
+//
+//  ControladorMotorIMapa.m
+//  iGlobo
+//
+//  Created by Jesus Cagide on 05/12/11.
+//  Copyright (c) 2011 INEGI. All rights reserved.
+//
+
+#import "ControladorMotorIMapa.h"
+
+@implementation ControladorMotorIMapa
+
+@end
